aebddf0b7280b2e07e2d38683a1152fea47670b1
Matheus <matheusdiegoteixeira@gmail.com>
2023-03-02 15:50:46 -0300

    First Commit

---
+++ src/PrimeiroProgramaJava.java
@@ -0,0 +1,8 @@
+import model.Gato;
+
+public class PrimeiroProgramaJava {
+    public static void main(String[] args) {
+        Gato gato = new Gato("Mia","branco", 2);
+        System.out.println(gato);
+    }
+}
